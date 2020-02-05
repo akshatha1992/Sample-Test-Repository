4e08fc1ce5a62f99c9bbae35c05e66d210e120be
nayaka <nayaka@MGSN01075BLR.mshome.net>
2020-02-05 14:11:17 +0530

    First Commit

---
+++ pipeline_demo/src/pipeline_demo/SeleniumExample.java
@@ -0,0 +1,28 @@
+package pipeline_demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\AutoMax Framework\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+        driver.get(baseUrl); 
+        actualTitle = driver.getTitle();
+ 
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+        driver.close();
+
+	}
+
+}
